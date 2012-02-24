ad22391c162f4a8f59cf3128f53a265c287fef04
Tailor Cai <tailorcai@gmail.com>
2012-02-24 18:58:05 +0800

    add

---
+++ src/main/java/com/ganji/tungsten/replicator/applier/GanjiMcQueueApplier.java
@@ -0,0 +1,320 @@
+/**
+ * Tungsten Scale-Out Stack
+ * Copyright (C) 2007-2010 Continuent Inc.
+ * Contact: tungsten@continuent.org
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ *
+ * Initial developer(s): Teemu Ollakka
+ * Contributor(s): Robert Hodges
+ */
+
+package com.ganji.tungsten.replicator.applier;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+import net.spy.memcached.AddrUtil;
+import net.spy.memcached.ConnectionFactoryBuilder;
+import net.spy.memcached.DefaultConnectionFactory;
+import net.spy.memcached.MemcachedClient;
+
+import org.apache.log4j.Logger;
+import org.json.simple.JSONObject;
+
+import com.continuent.tungsten.replicator.ReplicatorException;
+import com.continuent.tungsten.replicator.applier.ApplierException;
+import com.continuent.tungsten.replicator.conf.ReplicatorRuntime;
+import com.continuent.tungsten.replicator.database.Database;
+import com.continuent.tungsten.replicator.database.DatabaseFactory;
+import com.continuent.tungsten.replicator.event.ReplDBMSEvent;
+import com.continuent.tungsten.replicator.event.ReplDBMSHeader;
+import com.continuent.tungsten.replicator.plugin.PluginContext;
+import com.continuent.tungsten.replicator.thl.CommitSeqnoTable;
+
+
+/**
+ * Implements an applier for MemcacheQueue. The GanjiMcQueueApplier applier This class defines a
+ * GanjiMcQueueApplier
+ * 
+ * @author <a href="mailto:caifeng@ganji.com">CaiFeng
+ *         </a>
+ * @version 1.0
+ */
+public class GanjiMcQueueApplier extends RowDataApplier
+{
+    private static Logger  logger        = Logger.getLogger(GanjiMcQueueApplier.class);
+
+    // Task management information.
+    private int            taskId;
+
+    // using MySQL table to store commit seqno table
+    // CaiFeng
+    protected CommitSeqnoTable        commitSeqnoTable     = null;
+    protected Database                conn                 = null;
+    protected Statement               statement            = null;
+    protected ReplicatorRuntime       runtime              = null;
+    
+    MemcachedClient 					mc_conn				 = null;
+    protected String					queue_addr			 = "";
+    protected String					queue_name			 = "noname";
+     
+    protected String                   db                   = null;
+    
+    public void setDb(String db)
+    {
+        this.db = db;
+    }
+
+    public void setQueueAddr(String mcQueueHost) {
+		queue_addr = mcQueueHost;
+	}
+
+	public void setQueueName(String mcQueueName) {
+		queue_name = mcQueueName;
+	}
+	
+    public Database getDatabase()
+    {
+        return conn;
+    }
+
+    private boolean insert2Queue(String schema, String table, String actionName, JSONObject obj ) throws ApplierException
+    {
+        obj.put( "__schema", schema );
+        obj.put( "__table", table );
+        obj.put( "__action", actionName );
+        
+        Future<Boolean> f = mc_conn.set( queue_name, 0, obj.toJSONString() );
+        try {
+			Boolean b = f.get();
+			return b.booleanValue();
+		} catch (InterruptedException e) {
+			throw new ApplierException(e);
+		} catch (ExecutionException e) {
+			throw new ApplierException(e);
+		}
+        
+		//return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see com.continuent.tungsten.replicator.applier.RawApplier#commit()
+     */
+    @Override
+    public void commit() throws ApplierException, InterruptedException
+    {
+        // If there's nothing to commit, go back.
+        if (this.latestHeader == null )
+            return;
+
+        try
+        {
+            // Add applied latency so that we can easily track how far back each
+            // partition is. If we don't have data we just put in zero.
+            long appliedLatency;
+            if (latestHeader instanceof ReplDBMSEvent)
+                appliedLatency = (System.currentTimeMillis() - ((ReplDBMSEvent) latestHeader)
+                        .getExtractedTstamp().getTime()) / 1000;
+            else
+                appliedLatency = 0;
+
+            updateCommitSeqno(latestHeader, appliedLatency);
+        }
+        catch (SQLException e)
+        {
+            throw new ApplierException("Unable to commit transaction: "
+                    + e.getMessage(), e);
+        }
+    }
+
+    private void updateCommitSeqno(ReplDBMSHeader header, long appliedLatency) throws SQLException
+    {
+        if (commitSeqnoTable == null)
+            return;
+        if (logger.isDebugEnabled())
+            logger.debug("Updating commit seqno to " + header.getSeqno());
+        commitSeqnoTable.updateLastCommitSeqno(taskId, header, appliedLatency);
+    }    
+    
+    /**
+     * {@inheritDoc}
+     * 
+     * @see com.continuent.tungsten.replicator.applier.RawApplier#getLastEvent()
+     */
+    @Override
+    public ReplDBMSHeader getLastEvent() throws ApplierException,
+            InterruptedException
+    {
+        if (commitSeqnoTable == null)
+            return null;
+
+        try
+        {
+            return commitSeqnoTable.lastCommitSeqno(taskId);
+        }
+        catch (SQLException e)
+        {
+            throw new ApplierException(e);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see com.continuent.tungsten.replicator.applier.RawApplier#rollback()
+     */
+    @Override
+    public void rollback() throws InterruptedException
+    {
+        // Does nothing for now.
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see com.continuent.tungsten.replicator.applier.RawApplier#setTaskId(int)
+     */
+    @Override
+    public void setTaskId(int id)
+    {
+        this.taskId = id;
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see com.continuent.tungsten.replicator.plugin.ReplicatorPlugin#configure(com.continuent.tungsten.replicator.plugin.PluginContext)
+     */
+    @Override
+    public void configure(PluginContext context) throws ReplicatorException,
+            InterruptedException
+    {
+        //this.serviceSchema = "tungsten_" + context.getServiceName();
+        runtime = (ReplicatorRuntime) context;
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see com.continuent.tungsten.replicator.plugin.ReplicatorPlugin#prepare(com.continuent.tungsten.replicator.plugin.PluginContext)
+     */
+    @Override
+    public void prepare(PluginContext context) throws ReplicatorException,
+            InterruptedException
+    {
+        // Connect to MongoDB.
+        if (logger.isDebugEnabled())
+        {
+//            logger.debug("Connecting to Q4M: url="
+//                    + url + ",user=" + user + ",password" + password);
+        }
+
+        try
+        {
+            
+            // Create the database.
+            conn = DatabaseFactory.createDatabase(
+            		runtime.getJdbcUrl(db),
+            		runtime.getJdbcUser(),
+            		runtime.getJdbcPassword()
+            		);
+            		//url, user, password);
+            conn.connect(false);
+            statement = conn.createStatement();
+            
+            commitSeqnoTable = new CommitSeqnoTable(conn,
+                    db,
+                    runtime.getTungstenTableType(),
+                    false);
+            commitSeqnoTable.prepare(taskId);
+            latestHeader = commitSeqnoTable.lastCommitSeqno(taskId);
+            
+        }
+        catch (Exception e)
+        {
+            throw new ReplicatorException(
+                    "Unable to connect to MySQL: url="
+                    + runtime.getJdbcUrl(db) 
+                    + ",user=" + runtime.getJdbcUser() 
+                    + ",password" + runtime.getJdbcPassword(), e);
+        }
+
+        
+        try {
+        	mc_conn = new MemcachedClient(
+        			new ConnectionFactoryBuilder().
+        				//setFailureMode(fm).
+        				setOpTimeout(30*1000).
+        				build(),
+        		    AddrUtil.getAddresses( queue_addr ));
+        	
+        }
+        catch( Exception e)
+        {
+            throw new ReplicatorException(
+                    "Unable to connect to Memcached: url="
+                    + runtime.getJdbcUrl(db) 
+                    + ",user=" + runtime.getJdbcUser() 
+                    + ",password" + runtime.getJdbcPassword(), e);        	
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see com.continuent.tungsten.replicator.plugin.ReplicatorPlugin#release(com.continuent.tungsten.replicator.plugin.PluginContext)
+     */
+    @Override
+    public void release(PluginContext context) throws ReplicatorException,
+            InterruptedException
+    {
+        if (commitSeqnoTable != null)
+        {
+            commitSeqnoTable.release();
+            commitSeqnoTable = null;
+        }
+
+        //currentOptions = null;
+
+        statement = null;
+        if (conn != null)
+        {
+            conn.close();
+            conn = null;
+        }
+    }
+
+	@Override
+	protected void onRowData(String schema, String table, int aCTIONINSERT, JSONObject doc) throws ApplierException {
+		if( aCTIONINSERT == ACTION_INSERT) {
+			insert2Queue( schema, table, "insert", doc );
+		}
+		else if(aCTIONINSERT == ACTION_UPDATE) {
+			insert2Queue( schema, table, "update", doc );
+		}
+		else if(aCTIONINSERT == ACTION_DELETE) {
+			insert2Queue( schema, table, "delete", doc );
+		}
+		
+	}
+}
---
+++ src/main/java/com/ganji/tungsten/replicator/applier/RowDataApplier.java
@@ -0,0 +1,263 @@
+package com.ganji.tungsten.replicator.applier;
+
+import java.sql.Types;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.sql.rowset.serial.SerialException;
+
+import org.apache.log4j.Logger;
+import org.json.simple.JSONObject;
+
+import com.continuent.tungsten.replicator.ReplicatorException;
+import com.continuent.tungsten.replicator.applier.ApplierException;
+import com.continuent.tungsten.replicator.applier.RawApplier;
+import com.continuent.tungsten.replicator.consistency.ConsistencyException;
+import com.continuent.tungsten.replicator.dbms.DBMSData;
+import com.continuent.tungsten.replicator.dbms.LoadDataFileFragment;
+import com.continuent.tungsten.replicator.dbms.OneRowChange;
+import com.continuent.tungsten.replicator.dbms.RowChangeData;
+import com.continuent.tungsten.replicator.dbms.RowIdData;
+import com.continuent.tungsten.replicator.dbms.StatementData;
+import com.continuent.tungsten.replicator.dbms.OneRowChange.ColumnSpec;
+import com.continuent.tungsten.replicator.dbms.OneRowChange.ColumnVal;
+import com.continuent.tungsten.replicator.dbms.RowChangeData.ActionType;
+import com.continuent.tungsten.replicator.event.DBMSEmptyEvent;
+import com.continuent.tungsten.replicator.event.DBMSEvent;
+import com.continuent.tungsten.replicator.event.ReplDBMSHeader;
+import com.continuent.tungsten.replicator.extractor.mysql.SerialBlob;
+import com.continuent.tungsten.replicator.plugin.PluginContext;
+
+public abstract class RowDataApplier implements RawApplier {
+
+	private static Logger  logger        = Logger.getLogger(RowDataApplier.class);
+	
+	protected ReplDBMSHeader latestHeader;
+	
+	protected static int ACTION_INSERT = 0;
+	protected static int ACTION_UPDATE = 1;
+	protected static int ACTION_DELETE = 2;
+	
+    /**
+     * Applies row updates. Statements are discarded. {@inheritDoc}
+     * 
+     * @see com.continuent.tungsten.replicator.applier.RawApplier#apply(com.continuent.tungsten.replicator.event.DBMSEvent,
+     *      com.continuent.tungsten.replicator.event.ReplDBMSHeader, boolean)
+     */
+    @Override
+    public void apply(DBMSEvent event, ReplDBMSHeader header, boolean doCommit, boolean doRollback)
+            throws ReplicatorException, ConsistencyException, InterruptedException
+    {
+        ArrayList<DBMSData> dbmsDataValues = event.getData();
+
+        // COPY from JdbcApplier
+        if (latestHeader != null && latestHeader.getLastFrag()
+                && latestHeader.getSeqno() >= header.getSeqno()
+                && ! (event instanceof DBMSEmptyEvent))
+        {
+            logger.info("Skipping over previously applied event: seqno="
+                    + header.getSeqno() + " fragno=" + header.getFragno());
+            return;
+        }
+        if (logger.isDebugEnabled())
+            logger.debug("Applying event: seqno=" + header.getSeqno()
+                    + " fragno=" + header.getFragno() + " commit=" + doCommit);
+        
+        // Iterate through values inferring the database name.
+        for (DBMSData dbmsData : dbmsDataValues)
+        {
+            if (dbmsData instanceof StatementData)
+            {
+                if (logger.isDebugEnabled())
+                    logger.debug("Ignoring statement");
+            }
+            else if (dbmsData instanceof RowChangeData)
+            {
+                RowChangeData rd = (RowChangeData) dbmsData;
+                for (OneRowChange orc : rd.getRowChanges())
+                {
+                    ActionType action = orc.getAction();
+                    String schema = orc.getSchemaName();
+                    String table = orc.getTableName();
+                    
+//                    if( !filterSchemaTable( schema, table ) ) {
+//                        logger.debug("Processing row update: filtered out=" + action
+//                                + " schema=" + schema + " table=" + table);                    	
+//                    	continue;
+//                    }
+//                    if( exc_matcher != null ) {
+//                        exc_matcher.reset( schema + "." + table );
+//                        if( exc_matcher.matches() ) {
+//        				    continue;
+//        				}
+//                    }
+//                    
+//                    if( inc_matcher != null ) {
+//                        inc_matcher.reset( schema + "." + table );
+//                        if( !inc_matcher.matches() ) {
+//                            continue;
+//                        }
+//                    }
+
+                    if (logger.isDebugEnabled())
+                    {
+                        logger.debug("Processing row update: action=" + action
+                                + " schema=" + schema + " table=" + table);
+                    }
+
+                    if (action.equals(ActionType.INSERT))
+                    {
+                        // Fetch column names.
+                        List<ColumnSpec> colSpecs = orc.getColumnSpec();
+
+                        // Make a document and insert for each row.
+                        Iterator<ArrayList<ColumnVal>> colValues = orc
+                                .getColumnValues().iterator();
+                        while (colValues.hasNext())
+                        {
+                            JSONObject doc = new JSONObject();
+                            ArrayList<ColumnVal> row = colValues.next();
+                            for (int i = 0; i < row.size(); i++)
+                            {
+                                String name = colSpecs.get(i).getName();
+                                String value = ColValue2String(colSpecs.get(i), row.get(i));
+                                doc.put(name, value);
+                            }
+                            //insert2Queue( schema, table , "insert", doc );
+                            onRowData(schema, table, ACTION_INSERT, doc);
+                        }
+                           
+                    }
+                    else if (action.equals(ActionType.UPDATE))
+                    {
+                        // Fetch key and column names.
+                        //List<ColumnSpec> keySpecs = orc.getKeySpec();
+                        List<ColumnSpec> colSpecs = orc.getColumnSpec();
+                        ArrayList<ArrayList<OneRowChange.ColumnVal>> keyValues = orc
+                                .getKeyValues();
+                        ArrayList<ArrayList<OneRowChange.ColumnVal>> columnValues = orc
+                                .getColumnValues();
+
+                        // Iterate across the rows.
+                        for (int row = 0; row < columnValues.size()
+                                || row < keyValues.size(); row++)
+                        {
+                            JSONObject doc = new JSONObject();
+                            //List<ColumnVal> keyValuesOfRow = keyValues.get(row);
+                            List<ColumnVal> colValuesOfRow = columnValues.get(row);
+                            for (int i = 0; i < colValuesOfRow.size(); i++)
+                            {
+                                String name = colSpecs.get(i).getName();
+                                String value = ColValue2String(colSpecs.get(i), colValuesOfRow.get(i));
+                                doc.put(name, value);
+                            }
+                            onRowData(schema, table, ACTION_UPDATE, doc);
+                        }
+                    }
+                    else if (action.equals(ActionType.DELETE))
+                    {
+                        // Fetch key and column names.
+                        List<ColumnSpec> keySpecs = orc.getKeySpec();
+                        ArrayList<ArrayList<OneRowChange.ColumnVal>> keyValues = orc
+                                .getKeyValues();
+                        //List<ColumnSpec> colSpecs = orc.getColumnSpec();
+                        ArrayList<ArrayList<OneRowChange.ColumnVal>> columnValues = orc
+                                .getColumnValues();
+
+                        // Iterate across the rows.
+                        for (int row = 0; row < columnValues.size()
+                                || row < keyValues.size(); row++)
+                        {
+                            List<ColumnVal> keyValuesOfRow = keyValues.get(row);
+                            JSONObject doc = new JSONObject();
+                            
+                            // Prepare key values query to search for rows.
+                            for (int i = 0; i < keyValuesOfRow.size(); i++)
+                            {
+                                String name = keySpecs.get(i).getName();
+                                String value = ColValue2String(keySpecs.get(i), keyValuesOfRow.get(i) );
+                                doc.put(name, value);
+                            }
+                            onRowData(schema, table, ACTION_DELETE, doc);
+                        }
+                    }
+                    else
+                    {
+                        logger.warn("Unrecognized action type: " + action);
+                        return;
+                    }
+                    
+                }
+            }
+            else if (dbmsData instanceof LoadDataFileFragment)
+            {
+                if (logger.isDebugEnabled())
+                    logger.debug("Ignoring load data file fragment");
+            }
+            else if (dbmsData instanceof RowIdData)
+            {
+                if (logger.isDebugEnabled())
+                    logger.debug("Ignoring row ID data");
+            }
+            else
+            {
+                logger.warn("Unsupported DbmsData class: "
+                        + dbmsData.getClass().getName());
+            }
+        }
+
+        // Mark the current header and commit position if requested.
+        this.latestHeader = header;
+        if (doCommit)
+            commit();
+    }
+
+	abstract protected void onRowData(String schema, String table, int aCTIONINSERT,
+			JSONObject doc)throws ApplierException;
+
+
+	@Override
+	public void release(PluginContext arg0) throws ReplicatorException,
+			InterruptedException {
+		// TODO Auto-generated method stub
+
+	}
+	
+//	abstract protected boolean filterSchemaTable(String schema, String table);
+
+    private String ColValue2String(ColumnSpec cspec, ColumnVal cvalue) {
+        String value = null;
+        // BUG: if input is null , return null
+        if( cvalue.getValue() == null )
+        	return null;
+        logger.debug( "type=" + String.valueOf(cspec.getType()) +
+        		",length=" + String.valueOf(cspec.getLength()) +
+        		",value class=" + cvalue.getValue().getClass().toString());
+    
+        if( cspec.getType() == Types.VARCHAR ) {
+            value = new String( (byte[]) cvalue.getValue() );
+        }
+        else if( cspec.getType() == Types.BLOB ) {
+            SerialBlob blob = (SerialBlob) cvalue.getValue();
+            try
+            {
+                value = new String( blob.getBytes((long)1, (int) blob.length()));
+            }
+            catch (SerialException e)
+            {
+                value = "ERROR";
+            }
+        }
+//        else if( cspec.getType() == Types.INTEGER && cspec.getLength() == 8 ) {
+//        	
+//        }
+        else {
+            if( cvalue.getValue() != null ) 
+                value = cvalue.getValue().toString();
+            else
+                value = null;
+        }
+        return value;
+    }
+}
